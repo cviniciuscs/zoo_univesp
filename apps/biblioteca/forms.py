d9951c4753275546048d4ed3284d1a17d82a6c24
cviniciuscs <caio.constante@outlook.com>
2023-11-15 23:44:32 -0300

    caio

--- apps/biblioteca/forms.py
+++ apps/biblioteca/forms.py
@@ -4,27 +4,36 @@ from apps.biblioteca.models import Livro
 class FotografiaForms(forms.ModelForm):
     class Meta:
         model = Livro
-        exclude = ['publicada',]
+        exclude = ['publicada', 'usuario', ]
         labels = {
-            "observação": 'Observação',
-            'data_fotografia': "Data de Registro",
+            'observação': 'Observação',
+            'data_entrada': "Data de Registro",
             'usuario': "Usuário",
+            'nome_popular': 'Nome Popular',
+            'data_saida': 'Data Saída',
             }
         widgets = {
+            "nome_popular": forms.TextInput(attrs={"class": "form-control"}),
             "nome": forms.TextInput(attrs={"class": "form-control"}),
             "especie": forms.TextInput(attrs={"class": "form-control"}),
-            "nome_popular": forms.TextInput(attrs={"class": "form-control"}),
             "classe": forms.Select(attrs={"class": "form-control"}),
-            "Observação": forms.Textarea(attrs={"class": "form-control"}),
+            "observação": forms.Textarea(attrs={"class": "form-control"}),
             "sexo": forms.Select(attrs={"class": "form-control"}),
             "foto": forms.FileInput(attrs={"class": "form-control"}),
-            "usuario": forms.Select(attrs={"class": "form-control"}),
-            "data_fotografia": forms.DateInput(
+            "data_entrada": forms.DateInput(
                 format="%d/%m/%Y",
                 attrs={
                     "class": "form-control",
                     "type": "date",
-                }
+                }),
+            "origem": forms.TextInput(attrs={"class": "form-control"}),
+            "data_saida": forms.DateInput(
+                format="%d/%m/%Y",
+                attrs={
+                    "class": "form-control",
+                    "type": "date",
+                }),
+            "motivo": forms.TextInput(attrs={"class": "form-control"}
             ),
             
         }
--- apps/biblioteca/models.py
+++ apps/biblioteca/models.py
@@ -17,15 +17,18 @@ class Livro(models.Model):
         ("INDETERMINADO", "Indeterminado"),
         ]
 
+    nome_popular = models.CharField(max_length=150, null=False, blank=False)
     nome = models.CharField(max_length=100, null=False, blank=False)
     especie = models.CharField(max_length=100, null=False, blank=False, default=None)
-    nome_popular = models.CharField(max_length=150, null=False, blank=False)
     classe = models.CharField(max_length=100, choices=OPCAO_CATEGORIA, default="")
     observação = models.TextField(null=False, blank=False)
     foto = models.ImageField(upload_to='fotos/%Y/%m/%d/', blank=True)
     sexo = models.CharField(max_length=100, choices=OPCAO_SEXO, default="")
     publicada = models.BooleanField(default=False)
-    data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
+    data_entrada = models.DateTimeField(default=datetime.now, blank=False)
+    origem = models.CharField(max_length=100, null=False, blank=False)
+    data_saida = models.DateTimeField(default=datetime.now, blank=True, null=True)
+    motivo = models.CharField(max_length=100, null=False, blank=True)
     usuario = models.ForeignKey(
         to=User,
         on_delete=models.SET_NULL,
